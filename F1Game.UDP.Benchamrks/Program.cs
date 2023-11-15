bef98fcaa3217e081fcf6bc922cfe7d8072b3c90
Volodymyr Fed <volodymyr.fed94@gmail.com>
2023-11-15 16:03:52 +0200

    add read packet benchmark

---
+++ F1Game.UDP.Benchamrks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+using F122Benchmark;
+
+var config = DefaultConfig.Instance
+	.AddJob(Job.Default.WithId("NET7").WithRuntime(CoreRuntime.Core70))
+	.AddJob(Job.Default.WithId("NET8").WithRuntime(CoreRuntime.Core80));
+
+BenchmarkRunner.Run<ToPacketBenchmark>(config);
---
+++ F1Game.UDP.Benchamrks/ToPacketBenchmark.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+
+using BenchmarkDotNet.Attributes;
+
+using F1_22_UDP_Telemetry_Receiver.Packets;
+
+using F1Game.UDP;
+using F1Game.UDP.Packets;
+
+using SharpSessionHistoryPacket = F1Sharp.Packets.SessionHistoryPacket;
+
+namespace F122Benchmark;
+
+[MemoryDiagnoser(true)]
+public class ToPacketBenchmark
+{
+	readonly byte[] data;
+
+	public ToPacketBenchmark()
+	{
+		data = new byte[SessionHistoryDataPacket.Size];
+		new Random(42).NextBytes(data);
+		data[6] = 0;
+	}
+
+	[Benchmark]
+	public SessionHistoryDataPacket ReadSessionHistoryDataPacketMy()
+	{
+		var bytes = new BytesReader(data);
+		return bytes.GetNextObject<SessionHistoryDataPacket>();
+	}
+
+	[Benchmark]
+	public SharpSessionHistoryPacket ReadSessionHistoryPacketF1Sharp()
+	{
+		GCHandle handle = new();
+		try
+		{
+			handle = GCHandle.Alloc(data, GCHandleType.Pinned);
+			return (SharpSessionHistoryPacket)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(SharpSessionHistoryPacket))!;
+		}
+		finally
+		{
+			if (handle.IsAllocated)
+			{
+				handle.Free();
+			}
+		}
+	}
+
+	[Benchmark]
+	public PacketSessionHistoryData ReadPacketSessionHistoryDataF1Sim()
+	{
+		return new PacketSessionHistoryData(data);
+	}
+}
